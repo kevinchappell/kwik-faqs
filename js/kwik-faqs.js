a7356c05e9e25b469af0879c49e8fcb6c3de7714
Kevin Chappell <kevin.b.chappell@gmail.com>
2015-05-13 15:21:40 -0700

    various updates and bug fixes

--- js/kwik-faqs.js
+++ js/kwik-faqs.js
@@ -1,5 +1,12 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
 
-
+	$('.faqs h2 a').on('click', function(e) {
+		e.preventDefault();
+		var $answer = $(this).parents('.faqs').find('.entry-content');
+		console.debug($answer);
+		$answer.slideToggle(333, function(){
+			console.log('toggled');
+		});
+	});
 
 });
